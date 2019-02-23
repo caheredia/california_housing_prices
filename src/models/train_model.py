8d45947be73cce4796b6a0f2dab53ba4f976ca69
caheredia <cristianheredia@gmail.com>
2019-02-23 00:52:38 -0800

    update logging info and save location

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,20 +1,26 @@
 from sklearn.externals import joblib
+import logging
+from sklearn.ensemble import RandomForestRegressor
 
 
-def main()
+def main():
+    """ Trains model using processed data
+        Saves model (saved in ../models).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('traning model from processed data')
 
+    # Load saved transformed data
+    housing_prepared = joblib.load('data/processed/'+'housing_prepared'+'.pkl')
+    housing_labels = joblib.load('data/processed/'+'housing_labels'+'.pkl')
+    strat_test_set = joblib.load('data/processed/'+'strat_test_set'+'.pkl')
 
-# Load saved transformed data
-housing_prepared = joblib.load('data/interim/'+'housing_prepared'+'.pkl')
-housing_labels = joblib.load('data/interim/'+'housing_labels'+'.pkl')
-strat_test_set = joblib.load('data/processed/'+'strat_test_set'+'.pkl')
+    # Train model
+    forest_reg = RandomForestRegressor(n_estimators=30, max_features=8, random_state=42)
+    forest_reg.fit(housing_prepared, housing_labels)
 
-# Train model
-forest_reg = RandomForestRegressor(n_estimators=30, max_features=8, random_state=42)
-forest_reg.fit(housing_prepared, housing_labels)
-
-# Save model
-joblib.dump(forest_reg, "models/forest_model.pkl", compress=True)
+    # Save model
+    joblib.dump(forest_reg, "models/forest_model.pkl", compress=True)
 
 
 if __name__ == '__main__':
