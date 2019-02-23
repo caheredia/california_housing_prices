5940dcf933067f8a5f618d73e6e8eff9bc37fbd8
caheredia <cristianheredia@gmail.com>
2019-02-23 15:48:39 -0800

    fix string formatting

--- src/models/cost_estimator.py
+++ src/models/cost_estimator.py
@@ -10,11 +10,30 @@ from sklearn.ensemble import RandomForestRegressor
 full_pipeline = joblib.load('models/full_pipeline.pkl')
 forest_reg = joblib.load("models/forest_model.pkl")
 
+# columns for test data
+columns = ['longitude',
+           'latitude',
+           'housing_median_age',
+           'total_rooms',
+           'total_bedrooms',
+           'population',
+           'households',
+           'median_income',
+           'ocean_proximity']
+
 print('Enter house parameters:')
 print('For example:\n longitude|latitude|housing_median_age|total_rooms|total_bedrooms|population|households|median_income|ocean_proximity')
 print('As: -122.23,37.88,41.0,880.0,129.0,322.0,126.0,8.3252,"NEAR BAY"')
-x = input()
-x = np.array(x).reshape(-1, 1)
-x_prepared = full_pipeline.transform(x)
-#prediction = forest_reg.predict(x_prepared)
-print(x_prepared)
+x = input().split(',')
+# convert to arrray and format data for transform
+data = []
+for item in x:
+    try:
+        data.append(float(item))
+    except:
+        data.append(item[1:-1])
+test = pd.DataFrame(data).T
+test.columns = columns
+x_prepared = full_pipeline.transform(test)
+prediction = forest_reg.predict(x_prepared)
+print(prediction)
