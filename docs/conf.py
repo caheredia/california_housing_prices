1b67118436129451fdb21fc1fc8e1e95d04d585d
caheredia <cristianheredia@gmail.com>
2019-02-25 01:12:50 -0800

    add location for functions

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ from recommonmark.transform import AutoStructify
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'src'))
 
 # -- General configuration -----------------------------------------------------
 
