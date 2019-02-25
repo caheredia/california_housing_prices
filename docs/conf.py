02dde2314066cfa6c26a3fb22011e91564b2b7f0
caheredia <cristianheredia@gmail.com>
2019-02-25 00:11:11 -0800

    add extensinos

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ from recommonmark.transform import AutoStructify
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary',
+              'sphinx.ext.viewcode', 'nbsphinx', 'sphinx.ext.mathjax', 'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
