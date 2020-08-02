25cca7279478b5d6dd768686be4e4818eacabeae
Cristian Heredia <cheredia@wattlearn.com>
2020-08-02 15:27:25 -0700

    add basic logger

--- src/data/helpers.py
+++ src/data/helpers.py
@@ -1,7 +1,13 @@
 import logging
 
+logging.basicConfig(
+    format="ts :%(asctime)s, module %(module)s, lineno : %(lineno)d, message: %(message)s",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
+
 logger.info("making final data set from raw data")
 
 file_name = "data/raw/housing.csv"
+print(file_name)
 
